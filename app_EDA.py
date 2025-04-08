8fcda779a34fc08189fbcbae5dd3ec454bdc0679
Daniella <123836006+Darally06@users.noreply.github.com>
2025-04-07 22:50:24 -0500

    Update app_EDA.py

--- app_EDA.py
+++ app_EDA.py
@@ -4,8 +4,6 @@
 # Librerias
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 from unidecode import unidecode
 import folium
